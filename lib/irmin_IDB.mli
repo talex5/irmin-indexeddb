96f889153569d3990deaddbd64b9b87b4e359e00
Thomas Leonard <talex5@gmail.com>
2019-12-31 10:14:16 +0000

    Remove AO and RW functors
    These probably aren't used anywhere. Any code using these would need
    to do its own migration as they no longer refer to the old stores.

--- lib/irmin_IDB.mli
+++ lib/irmin_IDB.mli
@@ -3,14 +3,9 @@
 
 (** An Irmin backend that stores data in an IndexedDB. *)
 
-(* [config dn_name] store all values in the given IndexedDB database. *)
+(* [config db_name] store all values in the given IndexedDB database. *)
 val config : string -> Irmin.config
 
-module AO: Irmin.AO_MAKER
-module RW (K: Irmin.Hum.S) (V: Irmin.Hash.S): sig
-  include Irmin.RRW with type key = K.t and type value = V.t
-  val create: Irmin.config -> t Lwt.t
-end
 module Make (C: Irmin.Contents.S) (R: Irmin.Ref.S) (H: Irmin.Hash.S) : sig
   include Irmin.S
     with type key = C.Path.t
