a262904fc2bf0b15fe0b53acdd9319d949958b3d
Patrick Ferris <pf341@patricoferris.com>
2021-07-05 17:16:34 +0100

    use new lowercase module types

--- lib/irmin_indexeddb.mli
+++ lib/irmin_indexeddb.mli
@@ -7,10 +7,10 @@
 val config : string -> Irmin.config
 (** [config db_name] is a configuration that stores all values in the given IndexedDB database. *)
 
-module Content_store : Irmin.CONTENT_ADDRESSABLE_STORE_MAKER
+module Content_store : Irmin.Content_addressable.Maker
 (** The content-addressable store stores blobs, trees and commits. *)
 
-module Branch_store : Irmin.ATOMIC_WRITE_STORE_MAKER
+module Branch_store : Irmin.Atomic_write.Maker
 (** The branch store records the head commit hash for each branch. *)
 
 exception Format_too_old of [`Irmin_0_10]
